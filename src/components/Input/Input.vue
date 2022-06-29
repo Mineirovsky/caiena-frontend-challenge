cbb4f4ec2ba7a53a52d7a74ef94c29e6547ee66d
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2022-06-29 07:23:44 -0300

    Create Input component

---
+++ src/components/Input/Input.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps({
+  modelValue: String
+})
+const emit = defineEmits(['update:modelValue'])
+
+function handleInput (payload: Event) {
+  emit('update:modelValue', (payload.target as HTMLInputElement).value)
+}
+</script>
+
+<template>
+  <input
+    :value="modelValue"
+    @input="handleInput"
+    class="p-1 rounded border border-gray-200 m-2 outline-none focus:shadow transition-shadow duration-200"
+  >
+</template>
