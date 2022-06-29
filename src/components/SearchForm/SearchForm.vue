d6bb3f14855f0544cd283d1282bcf875a786c59b
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2022-06-29 17:53:22 -0300

    Fix form not submitting

--- src/components/SearchForm/SearchForm.vue
+++ src/components/SearchForm/SearchForm.vue
@@ -2,14 +2,14 @@
 import { ref } from 'vue'
 import { SearchIcon } from 'vue-tabler-icons'
 
-import Input from './Input'
-import Button from './Button.vue'
+import Input from '../Input'
+import Button from '../Button.vue'
 
 const emit = defineEmits(['submit'])
 const query = ref('')
 
 function handleSubmit () {
-  emit('submit', query)
+  emit('submit', query.value)
 }
 </script>
 
@@ -19,7 +19,7 @@ function handleSubmit () {
     class="flex flex-row items-stretch"
   >
     <Input v-model="query" class="rounded-r-none peer w-full" />
-    <Button type="submit" class="rounded-l-none peer-focus:shadow duration-200">
+    <Button type="submit" @click="handleSubmit" class="rounded-l-none peer-focus:shadow duration-200">
       <SearchIcon />
     </Button>
   </form>
