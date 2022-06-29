a275177034ece77ae4fba86ff65741340bc27431
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2022-06-28 22:23:12 -0300

    Setup Vue router

---
+++ src/routes.ts
@@ -0,0 +1,8 @@
+import { RouteRecordRaw } from 'vue-router'
+import Home from './pages/Home.vue'
+
+export const routes: RouteRecordRaw[] = [
+  { path: '/', component: Home }
+]
+
+export default routes
