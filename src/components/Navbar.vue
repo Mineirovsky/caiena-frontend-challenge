31a1ab6af67ff37a2bdc8d11bcfa1462e24224d2
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2022-06-29 18:01:20 -0300

    Create Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import SearchForm from './SearchForm'
+</script>
+
+<template>
+  <nav class="p-4 flex items-center shadow-lg shadow-sky-50">
+    <h1 class="uppercase text-sm grow-0 shrink-0">Github Explorer</h1>
+    <div class="w-full">
+      <SearchForm class="mx-auto max-w-lg" />
+    </div>
+  </nav>
+</template>
