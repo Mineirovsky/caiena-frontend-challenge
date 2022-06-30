479e39fbded92923135cc8d3bc704aa155dda849
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2022-06-29 22:45:53 -0300

    Add anchor to application title

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -11,7 +11,11 @@ async function handleSubmit (query: string) {
 
 <template>
   <nav class="p-4 flex items-center shadow-lg shadow-sky-50">
-    <h1 class="uppercase text-sm grow-0 shrink-0">Github Explorer</h1>
+    <h1 class="uppercase text-sm grow-0 shrink-0">
+      <RouterLink to="/">
+        Github Explorer
+      </RouterLink>
+    </h1>
     <div class="w-full">
       <SearchForm class="mx-auto max-w-lg" @submit="handleSubmit" />
     </div>
