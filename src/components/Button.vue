38e5dbf3caab744819233eb68f436fd5014c9de4
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2022-06-29 22:57:17 -0300

    Add color variations to button

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
 import { PropType } from 'vue'
 
-export type ButtonSizes = 'xs' | 'sm' | 'md' | 'lg' | 'xl'
+export type ButtonSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl'
+export type ButtonColor = 'simple' | 'primary' | 'success' | 'error'
 
 defineProps({
-  size: { type: String as PropType<ButtonSizes>, default: 'md' }
+  color: { type: String as PropType<ButtonColor>, default: 'simple' },
+  size: { type: String as PropType<ButtonSize>, default: 'md' }
 })
 
-const sizes = {
+const colors: Record<ButtonColor, string> = {
+  simple: 'bg-gray-100',
+  primary: 'bg-sky-700 text-white',
+  success: 'bg-emerald-400',
+  error: 'bg-red-700 text-white'
+}
+
+const sizes: Record<ButtonSize, string> = {
   xs: 'text-xs px-2 py-1',
   sm: 'text-sm px-3 py-2',
   md: 'px-4 py-2',
@@ -17,7 +26,7 @@ const sizes = {
 </script>
 
 <template>
-  <button :class="`bg-gray-100 rounded ${sizes[size]}`">
+  <button :class="`rounded ${colors[color]} ${sizes[size]}`">
     <slot />
   </button>
 </template>
