91edfee022fcd9edc182647eeda665f12987d30d
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2022-06-30 08:41:13 -0300

    Add pagination to search result

--- src/pages/SearchResult.vue
+++ src/pages/SearchResult.vue
@@ -1,30 +1,52 @@
 <script setup lang="ts">
-import { onMounted, ref, watch } from 'vue'
+import { computed, onMounted, ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
+
 import SearchResultItem from '../components/SearchResultItem'
-import { GithubUserSearchItem, makeSearchGithubUser } from '../repositories/searchGithubUser'
+import Pagination from '../components/Pagination'
+import { GithubUserSearchResponse, makeSearchGithubUser } from '../repositories/searchGithubUser'
 
 const searchGithubUser = makeSearchGithubUser(fetch)
 
 const route = useRoute()
-const results = ref([] as GithubUserSearchItem[])
+const result = ref<GithubUserSearchResponse | null>(null)
+const perPage = ref(20)
+const currentPage = ref(1)
 
-async function updateResults (query: string) {
-  results.value = (await searchGithubUser(query)).items
+async function updateResults () {
+  const query = String(route.query.q)
+  result.value = await searchGithubUser(query, {
+    page: currentPage.value,
+    per_page: perPage.value
+  })
 }
 
-watch(() => route.query.q, (value) => updateResults(value ? String(value) : 'octocat'))
+const pageCount = computed(
+  () => {
+    if (result.value === null) return 0
+    const totalItems = result.value.total_count <= 1000
+      ? result.value.total_count
+      : 1000
+    return Math.ceil(totalItems / perPage.value)
+  }
+)
 
-onMounted(() => updateResults(String(route.query.q)))
+watch(
+  [() => route.query.q, perPage, currentPage],
+  () => updateResults()
+)
+
+onMounted(() => updateResults())
 </script>
 
 <template>
   <main class="p-2">
     <h2 class="text-xl">Resultados para "{{ $route.query.q }}"</h2>
-    <ul v-if="results.length" class="flex flex-col items-center">
-      <li v-for="item in results" :key="item.id" class="my-2 w-1/2">
+    <ul v-if="result !== null" class="flex flex-col items-center">
+      <li v-for="item in result.items" :key="item.id" class="my-2 w-1/2">
         <SearchResultItem :user="item" />
       </li>
     </ul>
+    <Pagination v-model="currentPage" :page-count="pageCount" />
   </main>
 </template>
