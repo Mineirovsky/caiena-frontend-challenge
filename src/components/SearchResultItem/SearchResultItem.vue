73cf3c3ce0c5ebfae71ca94798cd55e1f00911d2
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2022-06-29 21:38:41 -0300

    Adjust spacing and font size

--- src/components/SearchResultItem/SearchResultItem.vue
+++ src/components/SearchResultItem/SearchResultItem.vue
@@ -10,14 +10,14 @@ defineEmits(['readMore'])
 </script>
 
 <template>
-  <article class="flex p-2 border border-gray-100 rounded hover:shadow">
+  <article class="flex p-3 border border-gray-100 rounded hover:shadow">
     <figure class="rounded-full w-20 h-20 shrink-0 grow-0 mr-4">
       <a :href="user.html_url" target="_blank">
         <img :src="user.avatar_url" class="rounded-full bg-gray-500">
       </a>
     </figure>
     <div>
-      <h3>
+      <h3 class="text-xl mb-3">
         <a :href="user.html_url" target="_blank">
           {{ user.login }}
         </a>
