032cccfc3cda7deeea5dc7c0a2ec4f5da1371c5e
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2022-06-29 22:05:03 -0300

    Add size variations to Button

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,23 @@
+<script setup lang="ts">
+import { PropType } from 'vue'
+
+export type ButtonSizes = 'xs' | 'sm' | 'md' | 'lg' | 'xl'
+
+defineProps({
+  size: { type: String as PropType<ButtonSizes>, default: 'md' }
+})
+
+const sizes = {
+  xs: 'text-xs px-2 py-1',
+  sm: 'text-sm px-3 py-2',
+  md: 'px-4 py-2',
+  lg: 'text-lg px-4 py-2',
+  xl: 'text-xl px-6 py-3'
+}
+</script>
+
 <template>
-  <button class="px-4 py-2 bg-gray-100 rounded">
+  <button :class="`bg-gray-100 rounded ${sizes[size]}`">
     <slot />
   </button>
 </template>
