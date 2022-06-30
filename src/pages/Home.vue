19e8b8d68fa5fb3ec1760895b8828f3a042ec053
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2022-06-29 21:20:40 -0300

    Add instructions to the user on the Home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { SearchIcon } from 'vue-tabler-icons'
+import Button from '../components/Button.vue'
+</script>
 
 <template>
-  <h1>Home</h1>
+  <main class="p-2">
+    <h2 class="text-xl">Inicie uma busca</h2>
+
+    Insira palavras chaves no campo acima e clique em
+    <Button disabled>
+      <SearchIcon />
+    </Button>
+  </main>
 </template>
