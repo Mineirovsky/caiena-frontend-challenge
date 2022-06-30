257843865aa2c9a887452c35161567ba9473fa6d
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2022-06-29 22:43:48 -0300

    Add capability for Button to be a RouterLink if needed

--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -4,10 +4,11 @@ import { PropType } from 'vue'
 export type ButtonSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl'
 export type ButtonColor = 'simple' | 'primary' | 'success' | 'error'
 
-defineProps({
+const props = defineProps({
   color: { type: String as PropType<ButtonColor>, default: 'simple' },
   href: { type: String, default: null },
-  size: { type: String as PropType<ButtonSize>, default: 'md' }
+  size: { type: String as PropType<ButtonSize>, default: 'md' },
+  to: { type: String, default: null }
 })
 
 const colors: Record<ButtonColor, string> = {
@@ -24,13 +25,20 @@ const sizes: Record<ButtonSize, string> = {
   lg: 'text-lg px-4 h-11',
   xl: 'text-xl px-6 h-14'
 }
+
+function componentIs () {
+  if (props.href) return 'a'
+  if (props.to) return 'RouterLink'
+  return 'button'
+}
 </script>
 
 <template>
   <component
-    :class="`inline-block rounded ${colors[color]} ${sizes[size]}`"
-    :is="href ? 'a' : 'button'"
+    :class="`inline-block rounded cursor-pointer disabled:cursor-default ${colors[color]} ${sizes[size]}`"
+    :is="componentIs()"
     :href="href"
+    :to="to"
   >
     <div class="flex items-center h-full">
       <slot />
