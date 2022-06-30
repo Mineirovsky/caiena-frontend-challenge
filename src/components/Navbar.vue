df271cd3fd004b55938e1c32b37432b7a0c86e03
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2022-06-29 21:31:05 -0300

    Add handler to submit search query

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router'
 import SearchForm from './SearchForm'
+const router = useRouter()
+
+async function handleSubmit (query: string) {
+  const queryEncoded = encodeURIComponent(query)
+  router.push(`/s?q=${queryEncoded}`)
+}
 </script>
 
 <template>
   <nav class="p-4 flex items-center shadow-lg shadow-sky-50">
     <h1 class="uppercase text-sm grow-0 shrink-0">Github Explorer</h1>
     <div class="w-full">
-      <SearchForm class="mx-auto max-w-lg" />
+      <SearchForm class="mx-auto max-w-lg" @submit="handleSubmit" />
     </div>
   </nav>
 </template>
