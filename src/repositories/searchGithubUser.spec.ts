d89f3da4086a888819ed5b76b0ac9df835b9fe16
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2022-06-29 22:57:32 -0300

    Include test case for search request with options

--- src/repositories/searchGithubUser.spec.ts
+++ src/repositories/searchGithubUser.spec.ts
@@ -13,3 +13,18 @@ test('search calls correct URL', async () => {
   // @ts-ignore
   expect(fetchMock.mock.calls[0][0]).toContain('q=octocat')
 })
+
+test('search calls correct URL with options', async () => {
+  const fetchMock = vi.fn(() => new Promise((resolve) => resolve({ json: () => null })))
+
+  // @ts-ignore
+  const searchGithubUser = makeSearchGithubUser(fetchMock)
+
+  await searchGithubUser('octocat', { page: 2, per_page: 10 })
+
+  // @ts-ignore
+  const options = fetchMock.mock.calls[0][0]
+
+  expect(options).toContain('page=1')
+  expect(options).toContain('per_page=10')
+})
